4ae4d63b6936dcd745f5e8ad92a2704ba17f3fa4
Romain Landolfini <romain.landolfini@gmail.com>
2022-11-13 23:08:16 +0000

    :white_check_mark: added 4 passed tests to Goal class

--- run_tests.py
+++ run_tests.py
@@ -1,5 +1,6 @@
 import unittest
 
+from tests.goal_test import TestGoal
 
 if __name__ == '__main__':
     unittest.main()
--- tests/goal_test.py
+++ tests/goal_test.py
@@ -6,5 +6,17 @@ class TestGoal(unittest.TestCase):
 
     def setUp(self):
         self.goal1 = Goal("General fitness", 8, 9, 3)
-        #title, rep_start, rep_end, sets,
+        self.goal2 = Goal("Hypertrophy", 6, 12, 4)
         
+    def test_goal_has_title(self):
+        self.assertEqual("General fitness", self.goal1.title)
+
+    def test_goal_has_start_range(self):
+        self.assertEqual(6, self.goal2.rep_start)
+
+    def test_goal_has_end_range(self):
+        self.assertEqual(12, self.goal2.rep_end)
+    
+    def test_goal_has_set(self):
+        self.assertEqual(3, self.goal1.sets)
+    
