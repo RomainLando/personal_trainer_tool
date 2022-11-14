751d8fbff1915ff11f2731405bac5cf3c8a2abd8
Romain Landolfini <romain.landolfini@gmail.com>
2022-11-14 20:59:17 +0000

    :sparkles: client_program repository complete. all simple sql queries complete

--- console.py
+++ console.py
@@ -1,22 +1,24 @@
-import pdb
-
 from models.goal import Goal
 from models.exercise import Exercise
 from models.program import Program
 from models.workout import Workout
 from models.client import Client
+from models.client_program import ClientProgram
 
+import repositories.client_program_repository as client_program_repository
 import repositories.goal_repository as goal_repository
 import repositories.exercise_repository as exercise_repository
 import repositories.program_repository as program_repository
 import repositories.workout_repository as workout_repository
 import repositories.client_repository as client_repository
 
-goal_repository.delete_all()
+
+client_program_repository.delete_all()
 client_repository.delete_all()
-# workout_repository.delete_all()
-# program_repository.delete_all()
-# exercise_repository.delete_all()
+goal_repository.delete_all()
+workout_repository.delete_all()
+program_repository.delete_all()
+exercise_repository.delete_all()
 
 
 goal1 = Goal("General fitness", 8, 9, 3)
@@ -41,16 +43,16 @@ exercise8 = Exercise("Deadlifts", "Legs")
 exercise9 = Exercise("Lateral Raises", "Upper Body")
 exercise10 = Exercise("Rows", "Upper Body")
 
-# exercise_repository.save(exercise1)
-# exercise_repository.save(exercise2)
-# exercise_repository.save(exercise3)
-# exercise_repository.save(exercise4)
-# exercise_repository.save(exercise5)
-# exercise_repository.save(exercise6)
-# exercise_repository.save(exercise7)
-# exercise_repository.save(exercise8)
-# exercise_repository.save(exercise9)
-# exercise_repository.save(exercise10)
+exercise_repository.save(exercise1)
+exercise_repository.save(exercise2)
+exercise_repository.save(exercise3)
+exercise_repository.save(exercise4)
+exercise_repository.save(exercise5)
+exercise_repository.save(exercise6)
+exercise_repository.save(exercise7)
+exercise_repository.save(exercise8)
+exercise_repository.save(exercise9)
+exercise_repository.save(exercise10)
 
 
 program1 = Program("Legs")
@@ -58,10 +60,10 @@ program2 = Program("Push")
 program3 = Program("Pull")
 program4 = Program("Full Body")
 
-# program_repository.save(program1)
-# program_repository.save(program2)
-# program_repository.save(program3)
-# program_repository.save(program4)
+program_repository.save(program1)
+program_repository.save(program2)
+program_repository.save(program3)
+program_repository.save(program4)
 
 
 workout1 = Workout(program1, exercise1)
@@ -77,18 +79,18 @@ workout10 = Workout(program4, exercise2)
 workout11 = Workout(program4, exercise8)
 workout12 = Workout(program4, exercise5)
 
-# workout_repository.save(workout1)
-# workout_repository.save(workout2)
-# workout_repository.save(workout3)
-# workout_repository.save(workout4)
-# workout_repository.save(workout5)
-# workout_repository.save(workout6)
-# workout_repository.save(workout7)
-# workout_repository.save(workout8)
-# workout_repository.save(workout9)
-# workout_repository.save(workout10)
-# workout_repository.save(workout11)
-# workout_repository.save(workout12)
+workout_repository.save(workout1)
+workout_repository.save(workout2)
+workout_repository.save(workout3)
+workout_repository.save(workout4)
+workout_repository.save(workout5)
+workout_repository.save(workout6)
+workout_repository.save(workout7)
+workout_repository.save(workout8)
+workout_repository.save(workout9)
+workout_repository.save(workout10)
+workout_repository.save(workout11)
+workout_repository.save(workout12)
 
 
 client1 = Client("Adrien", "Hughes", 42, 170, 85, goal1)
@@ -96,12 +98,24 @@ client2 = Client("Susan", "Boyle", 37, 160, 70, goal2)
 client3 = Client("Dominic", "Storm", 20, 191, 79, goal3)
 client4 = Client("Sarah", "Parker", 19, 150, 45, goal4)
 
-# client_repository.save(client1)
-# client_repository.save(client2)
-# client_repository.save(client3)
-# client_repository.save(client4)
+client_repository.save(client1)
+client_repository.save(client2)
+client_repository.save(client3)
+client_repository.save(client4)
 
 
+client_program1 = ClientProgram(client1, program4)
+client_program2 = ClientProgram(client2, program1)
+client_program3 = ClientProgram(client2, program2)
+client_program4 = ClientProgram(client2, program3)
+client_program5 = ClientProgram(client3, program1)
+client_program6 = ClientProgram(client4, program1)
+client_program7 = ClientProgram(client4, program4)
 
-
-pdb.set_trace()
+client_program_repository.save(client_program1)
+client_program_repository.save(client_program2)
+client_program_repository.save(client_program3)
+client_program_repository.save(client_program4)
+client_program_repository.save(client_program5)
+client_program_repository.save(client_program6)
+client_program_repository.save(client_program7)
--- repositories/client_program_repository.py
+++ repositories/client_program_repository.py
@@ -1,2 +1,67 @@
 from db.run_sql import run_sql
-from models.client import Client
+from models.client_program import ClientProgram
+
+from repositories import program_repository, client_repository
+
+def save(client_program):
+    sql = """INSERT INTO client_programs 
+    (client_id, program_id) 
+    VALUES (%s, %s) RETURNING id
+    """
+    values = [client_program.client.id, client_program.program.id]
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    client_program.id = id
+
+
+def select_all():
+    client_programs = []
+    sql = "SELECT * FROM client_programs"
+    results = run_sql(sql)
+    
+    for result in results:
+        client_id = result['client_id']
+        program_id = result['program_id']
+        client = client_repository.select(client_id)
+        program = program_repository.select(program_id)
+        client_program = ClientProgram(client, program, result['id'])
+        client_programs.append(client_program)
+    return client_programs
+
+def select(id):
+    client_program = None
+    sql = """
+    SELECT * FROM client_programs
+    WHERE id = %s
+    """
+    values = [id]
+    results = run_sql(sql, values)
+
+    if results:
+        result = results[0]
+        client_id = result['client_id']
+        program_id = result['program_id']
+        client = client_repository.select(client_id)
+        program = program_repository.select(program_id)
+        client_program = ClientProgram(client, program, result['id'])
+    return client_program
+
+def delete_all():
+    sql= "DELETE FROM client_programs"
+    run_sql(sql)
+
+def delete(id):
+    sql = """
+    DELETE FROM client_programs
+    WHERE id = %s
+    """
+    values = [id]
+    run_sql(sql, values)
+
+def update(client_program):
+    sql = """
+    UPDATE client_programs SET (client_id, program_id) = (%s, %s) 
+    WHERE id = %s
+    """
+    values = [client_program.client.id, client_program.program.id, client_program.id]
+    run_sql(sql, values)
