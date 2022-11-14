3fb118a875f82f8cb7990965dd592bb1a07a2d4c
Romain Landolfini <romain.landolfini@gmail.com>
2022-11-14 11:26:33 +0000

    :white_check_mark: added program tests

--- run_tests.py
+++ run_tests.py
@@ -2,6 +2,7 @@ import unittest
 
 from tests.goal_test import TestGoal
 from tests.exercise_test import TestExercise
+from tests.program_test import TestProgram
 
 if __name__ == '__main__':
     unittest.main()
--- tests/program_test.py
+++ tests/program_test.py
@@ -1,3 +1,14 @@
 import unittest
 
 from models.program import Program
+
+class TestProgram(unittest.TestCase):
+
+    def setUp(self):
+        self.program1 = Program("Legs")
+    
+    def test_program_has_title(self):
+        self.assertEqual("Legs", self.program1.title)
+    
+    def test_program_id_is_none(self):
+        self.assertIsNone(self.program1.id)
