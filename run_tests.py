3847b4661faca3d8322edc9a98c2ba554f40ddb7
Romain Landolfini <romain.landolfini@gmail.com>
2022-11-14 12:04:41 +0000

    :white_check_mark: added Client tests

--- run_tests.py
+++ run_tests.py
@@ -3,6 +3,7 @@ import unittest
 from tests.goal_test import TestGoal
 from tests.exercise_test import TestExercise
 from tests.program_test import TestProgram
+from tests.client_test import TestClient
 
 if __name__ == '__main__':
     unittest.main()
--- tests/client_test.py
+++ tests/client_test.py
@@ -4,4 +4,25 @@ from models.client import Client
 from models.goal import Goal
 
 class TestClient(unittest.TestCase):
-    pass
+    
+    def setUp(self):
+        self.goal1 = Goal("General fitness", 8, 9, 3)
+        self.client1 = Client("Adrien", "Hughes", 42, 170, 85, self.goal1)
+    
+    def test_client_has_first_name(self):
+        self.assertEqual("Adrien", self.client1.first_name)
+    
+    def test_client_has_last_name(self):
+        self.assertEqual("Hughes", self.client1.last_name)
+    
+    def test_client_has_age(self):
+        self.assertEqual(42, self.client1.age)
+    
+    def test_client_has_height(self):
+        self.assertEqual(170, self.client1.height)
+    
+    def test_cliient_has_weight(self):
+        self.assertEqual(85, self.client1.weight)
+    
+    def test_client_has_goal(self):
+        self.assertEqual(3, self.client1.goal.sets)
