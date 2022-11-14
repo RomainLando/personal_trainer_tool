97488d5afc22a5f2e8fb2c45a74738efd155fa26
Romain Landolfini <romain.landolfini@gmail.com>
2022-11-14 14:12:59 +0000

    :white_check_mark: Workout tests added, all tests done

--- run_tests.py
+++ run_tests.py
@@ -5,6 +5,7 @@ from tests.exercise_test import TestExercise
 from tests.program_test import TestProgram
 from tests.client_test import TestClient
 from tests.client_program_test import TestClientProgram
+from tests.workout_test import TestWorkout
 
 if __name__ == '__main__':
     unittest.main()
--- tests/workout_test.py
+++ tests/workout_test.py
@@ -4,3 +4,18 @@ from models.workout import Workout
 from models.program import Program
 from models.exercise import Exercise
 
+class TestWorkout(unittest.TestCase):
+
+    def setUp(self):
+        self.exercise1 = Exercise("Squats", "Legs")
+        self.program1 = Program("Legs")
+        self.workout1 = Workout(self.program1, self.exercise1)
+
+    def test_workout_has_exercise(self):
+        self.assertEqual("Squats", self.workout1.exercise.name)
+    
+    def test_workout_has_program(self):
+        self.assertEqual("Legs", self.workout1.program.title)
+    
+    def test_workout_id_is_none(self):
+        self.assertIsNone(self.workout1.id)
