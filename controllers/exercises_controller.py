d92268704a769e3fea409f130a8807d525d0f052
Romain Landolfini <romain.landolfini@gmail.com>
2022-11-15 18:40:25 +0000

    :construction: added new and delete to exercises controller

--- controllers/exercises_controller.py
+++ controllers/exercises_controller.py
@@ -11,6 +11,26 @@ def exercises():
     exercises = exercise_repository.select_all()
     return render_template("exercises/index.html", exercises=exercises)
 
+
 @exercises_blueprint.route("/exercises/new")
 def new_exercise():
     return render_template("exercises/new.html")
+
+
+@exercises_blueprint.route("/exercises", methods=["POST"])
+def create_exercise():
+    name = request.form["name"]
+    muscle_group = request.form["muscle_group"]
+    if request.form["duration"]:
+        duration = request.form["duration"]
+        new_exercise = Exercise(name, muscle_group, duration)
+    else:
+        new_exercise = Exercise(name, muscle_group)
+    exercise_repository.save(new_exercise)
+    return redirect("exercises")
+
+
+@exercises_blueprint.route("/exercises/<id>/delete", methods=["POST"])
+def delete_exercise(id):
+    exercise_repository.delete(id)
+    return redirect("/exercises")
