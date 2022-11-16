229df9e23469906e1e7fce26c6111d5193e194e5
Romain Landolfini <romain.landolfini@gmail.com>
2022-11-16 11:13:09 +0000

    :construction: clients controller started, index page functional

--- app.py
+++ app.py
@@ -2,12 +2,13 @@ from flask import Flask, render_template
 from controllers.exercises_controller import exercises_blueprint
 from controllers.goals_controller import goals_blueprint
 from controllers.programs_controller import programs_blueprint
-
+from controllers.clients_controller import clients_blueprint
 app = Flask(__name__)
 
 app.register_blueprint(exercises_blueprint)
 app.register_blueprint(goals_blueprint)
 app.register_blueprint(programs_blueprint)
+app.register_blueprint(clients_blueprint)
 
 @app.route("/")
 def main():
