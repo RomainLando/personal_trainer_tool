00ac5ed66b759d436f6d973fdca7ed5cd3dad240
Romain Landolfini <romain.landolfini@gmail.com>
2022-11-16 11:53:26 +0000

    :construction: updated specific clients controller and view

--- controllers/clients_controller.py
+++ controllers/clients_controller.py
@@ -10,3 +10,9 @@ clients_blueprint = Blueprint("clients", __name__)
 def clients():
     clients = client_repository.select_all()
     return render_template("/clients/index.html", clients=clients)
+
+@clients_blueprint.route("/clients/<id>")
+def show_client(id):
+    client = client_repository.select(id)
+    programs = client_repository.show_programs(client)
+    return render_template("clients/client.html", client = client, programs = programs)
