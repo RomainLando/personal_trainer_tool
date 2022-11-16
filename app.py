37eb7fd6a2d0a89e443b764a07c1a69a3dcdbeae
Romain Landolfini <romain.landolfini@gmail.com>
2022-11-16 00:13:01 +0000

    :construction: almost done with program, need to remove one exercise from workout at a time

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
 from controllers.exercises_controller import exercises_blueprint
 from controllers.goals_controller import goals_blueprint
+from controllers.programs_controller import programs_blueprint
 
 app = Flask(__name__)
 
 app.register_blueprint(exercises_blueprint)
 app.register_blueprint(goals_blueprint)
+app.register_blueprint(programs_blueprint)
 
 @app.route("/")
 def main():
---
+++ controllers/programs_controller.py
@@ -0,0 +1,65 @@
+from flask import Blueprint, redirect, render_template, request
+
+from models.program import Program
+from models.workout import Workout
+import repositories.program_repository as program_repository
+import repositories.exercise_repository as exercise_repository
+import repositories.workout_repository as workout_repository
+
+programs_blueprint = Blueprint("programs", __name__)
+
+@programs_blueprint.route("/programs")
+def programs():
+    programs = program_repository.select_all()
+    return render_template("/programs/index.html", programs=programs)
+
+@programs_blueprint.route("/programs/<id>")
+def show_program(id):
+    program = program_repository.select(id)
+    exercises = program_repository.show_exercises(program)
+    return render_template('programs/program.html', program=program, exercises=exercises)
+
+@programs_blueprint.route("/programs/new")
+def new_program():
+    exercises = exercise_repository.select_all()
+    return render_template("programs/new.html", exercises=exercises)
+
+@programs_blueprint.route("/programs", methods=["POST"])
+def create_program():
+    title = request.form["title"]
+    new_program = Program(title)
+    program_repository.save(new_program)
+    exercises_id = request.form.getlist('exercises_id')
+    for exercise_id in exercises_id:
+        new_exercise = exercise_repository.select(int(exercise_id))
+        new_workout = Workout(new_program, new_exercise)
+        workout_repository.save(new_workout)
+    return redirect("/programs")
+
+@programs_blueprint.route("/programs/<id>/delete", methods=["POST"])
+def delete_program(id):
+    program_repository.delete(id)
+    return redirect("/programs")
+
+@programs_blueprint.route("/programs/<id>/edit")
+def edit_program(id):
+    program = program_repository.select(id)
+    exercises = exercise_repository.select_all()
+    workout = program_repository.show_exercises(program)
+    workout_ids = [program_exercise.id for program_exercise in workout]
+    return render_template("programs/edit.html", program=program, exercises=exercises, workout_ids=workout_ids)
+
+@programs_blueprint.route("/programs/<id>", methods=["POST"])
+def update_program(id):
+    title = request.form["title"]
+    new_program = Program(title, id)
+    program_repository.update(new_program)
+    exercises = program_repository.show_exercises(new_program)
+    for exercise in exercises:
+        workout_repository.delete(exercise.id)
+    exercises_id = request.form.getlist('exercises_id')
+    for exercise_id in exercises_id:
+        new_exercise = exercise_repository.select(int(exercise_id))
+        new_workout = Workout(new_program, new_exercise)
+        workout_repository.save(new_workout)
+    return redirect("/programs")
